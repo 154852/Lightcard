8d1fd78eeeff126d00d541d7e1faec4a1884b7a2
154852 <louisemileploix@icloud.com>
2018-10-09 17:10:21 +0100

    Added multi-part QR codes

--- ASSETS/JS/qr.js
+++ ASSETS/JS/qr.js
@@ -1,21 +1,21 @@
 const main = document.getElementById("main");
 
-const width = Math.min(window.innerWidth, window.innerHeight - document.querySelector('header').getBoundingClientRect().height - 100) + 'px';
+const width = Math.min(window.innerWidth, window.innerHeight - document.querySelector('header').getBoundingClientRect().height - 100);
 
 const deck = APP.loadAllDecks()[parseInt(getParameterByName('i'))];
 const code = APP.genCodeForDeck(deck);
-for (var i = 0; i < code.length; i += 1000) {
+for (var i = 0; i < code.length; i += (width * 2)) {
 	const qrcode = new QRCode(main, {
 		width : 5000,
 		height : 5000
 	});
 	
-	const address = 'http://' + window.location.host + '/qr-import.html?i=' + (i / 1000) + '&part=' + code.substring(i, i + 1000);
+	const address = 'http://' + window.location.host + '/qr-import.html?i=' + (i / 1000) + '&part=' + code.substring(i, i + (width / 2));
 	console.log(address);
 	qrcode.makeCode(address);
 
-	main.lastChild.style.width = width;
-	main.lastChild.style.height = width;
+	main.lastChild.style.width = width + 'px';
+	main.lastChild.style.height = width + 'px';
 }
 
 main.title = '';
